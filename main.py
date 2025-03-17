797d46d4655e3ef42e3e5a63b3f140c84f1d4c16
azerly3 <azerly3@gmail.com>
2025-03-17 17:16:55 +0100

    Bookbot finalized

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words
 from stats import get_num_characters
 from stats import sorted_report
@@ -7,21 +8,25 @@ def get_book_text(filepath):
         return f.read()
      
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    word_count = get_num_words(text)
-    character_count = get_num_characters(text)
-    report = sorted_report(character_count)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
+    if len(sys.argv) >= 2:    
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        word_count = get_num_words(text)
+        character_count = get_num_characters(text)
+        report = sorted_report(character_count)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
 
-    print("----------- Word Count ----------")
-    print(f"Found {word_count} total words")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count} total words")
 
-    print("--------- Character Count -------")
-    for char, count in report:
-        print(f"{char}: {count}")
+        print("--------- Character Count -------")
+        for char, count in report:
+            print(f"{char}: {count}")
 
-    print("============= END ===============")
-    
+        print("============= END ===============")
+    else :
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
 main()
